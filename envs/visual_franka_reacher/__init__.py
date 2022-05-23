371943839a347d745268959e6a41ae7bc70773b4
ne-v0y <ne-v0y>
2022-05-23 15:16:21 -0600

    fix: maintain cycle time correctly

--- envs/visual_franka_reacher/__init__.py
+++ envs/visual_franka_reacher/__init__.py
@@ -2,7 +2,7 @@ from gym.envs.registration import register
 
 register(
     id='franka_agile_grasping-v0',
-    entry_point='envs.visual_ur5_reacher.reacher_env_visual:FrankaPanda_agile_grasping_V0',
+    entry_point='envs.visual_franka_reacher.reacher_env_visual:FrankaPanda_agile_grasping_V0',
 )
 
 # import envs.visual_ur5_reacher.reacher_env_visual
